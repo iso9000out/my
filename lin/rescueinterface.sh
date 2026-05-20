fba743f557ac2f1383bcf0feebbb320a78c80c3f
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-05-20 17:37:25 +0800

    Update rescueinterface.sh

--- lin/rescueinterface.sh
+++ lin/rescueinterface.sh
@@ -6,7 +6,18 @@
 
 # 订阅错乱抢救方案：
 
-# 拉取救急yaml代码写入app_20
+# 救火yaml代码写入app_20
 # /opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/ydy/mymv8" -o /opt/storage/app_20.sh && /bin/chmod +x /opt/storage/app_20.sh
 
+# 动态时钟变频：用写死的 curl
 # /opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/lin/croncctv" -o /opt/storage/cron/crontabs/cctv
+
+# 权限合规：直接裸写
+# chown cctv:root /opt/storage/cron/crontabs/cctv
+# chmod 600 /opt/storage/cron/crontabs/cctv
+
+# 锁死闪存 并写入 防止路由器重启后配置回滚
+# /sbin/mtd_storage.sh save
+
+# 5. 立即生效 新时间频率立刻爆发
+/usr/bin/killall crond && /usr/sbin/crond
