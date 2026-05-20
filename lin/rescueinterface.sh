b60a506793ebc4136cc2916da4b81184ea1a0889
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-05-20 17:06:53 +0800

    Update rescueinterface.sh

--- lin/rescueinterface.sh
+++ lin/rescueinterface.sh
@@ -8,3 +8,5 @@
 
 # 拉取救急yaml代码写入app_20
 # /opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/ydy/mymv8" -o /opt/storage/app_20.sh && /bin/chmod +x /opt/storage/app_20.sh
+
+# /opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/lin/croncctv" -o /opt/storage/cron/crontabs/cctv
