ca50cbaa0f9be520b99a89b1c013957e3ec28dcd
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-05-20 15:47:34 +0800

    Update rescueinterface.sh

--- lin/rescueinterface.sh
+++ lin/rescueinterface.sh
@@ -5,4 +5,6 @@
 # 本接口是抢救接口，平时保持空白，一旦出事故，填入抢救方案
 
 # 订阅错乱抢救方案：
+
+# 拉取救急yaml代码写入app_20
 # /opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/ydy/mymv8" -o /opt/storage/app_20.sh && /bin/chmod +x /opt/storage/app_20.sh
