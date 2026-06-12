35dcdbb28f8498afd213a0019a040bb5fdb4d9ba
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-06-12 13:31:17 +0800

    Update rescueinterface.sh

--- lin/rescueinterface.sh
+++ lin/rescueinterface.sh
@@ -2,7 +2,7 @@
 
 
 
-/opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/ydy/mymv8" -o /opt/storage/app_20.sh && /bin/chmod +x /opt/storage/app_20.sh
+/opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/lin/app20.sh" -o /opt/storage/app_20.sh && /bin/chmod +x /opt/storage/app_20.sh
 
 
 
@@ -14,7 +14,7 @@
 # 订阅错乱抢救方案：
 
 # 救火yaml代码写入app_20
-# /opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/ydy/mymv8" -o /opt/storage/app_20.sh && /bin/chmod +x /opt/storage/app_20.sh
+# /opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/lin/app20.sh" -o /opt/storage/app_20.sh && /bin/chmod +x /opt/storage/app_20.sh
 
 # 动态时钟变频：用写死的 curl
 # /opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/lin/croncctv" -o /opt/storage/cron/crontabs/cctv
