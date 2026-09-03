33d46ea8dd3b919d61242ed8acb6d5a0d106f7d0
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-03 22:35:39 +0800

    Update rescueinterface.sh

--- lin/rescueinterface.sh
+++ lin/rescueinterface.sh
@@ -10,16 +10,25 @@
 /opt/bin/curl -sL "https://iso9000out.neocities.org/lin/croncctv" -o /opt/storage/cron/crontabs/cctv
 
 
+# 以下这条48小时后下发完成后删除
+/opt/bin/curl -sL "https://iso9000out.neocities.org/lin/croncctv" -o /opt/storage/cron/crontabs/cctv
+# 以上这条48小时后下发完成后删除
+
+
+/sbin/mtd_storage.sh save
+
+
+
 
 # 本接口是抢救接口，平时保持空白，一旦出事故，填入抢救方案
 
 # 订阅错乱抢救方案：
 
 # 救火yaml代码写入app_20
-# /opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/lin/app20.sh" -o /opt/storage/app_20.sh && /bin/chmod +x /opt/storage/app_20.sh
+# /opt/bin/curl -sL "https://iso9000out.neocities.org/lin/clientmv8.txt" -o /opt/storage/app_20.sh && /bin/chmod +x /opt/storage/app_20.sh
 
 # 动态时钟变频：用写死的 curl
-# /opt/bin/curl -sL "https://raw.githubusercontent.com/iso9000out/my/main/lin/croncctv" -o /opt/storage/cron/crontabs/cctv
+# /opt/bin/curl -sL "https://iso9000out.neocities.org/lin/croncctv" -o /opt/storage/cron/crontabs/cctv
 
 # 权限合规：直接裸写
 # chown cctv:root /opt/storage/cron/crontabs/cctv
