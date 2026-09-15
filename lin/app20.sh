ce823807c912cbdeb563a4d9eb4c396c5a3c6989
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-16 05:48:55 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -510,9 +510,7 @@ proxy-groups:
     tolerance: 800
     max-failed-times: 3
     include-all-providers: true
-    #filter: (?i)^(🇺🇸|US|🇸🇬|SG|🇹🇭|🇲🇾|🇵🇭|🇻🇳|🇮🇳|🇯🇵|🇰🇷|🇳🇱|🇬🇧|🇩🇪|🇫🇷|🇷🇴|🇨🇦|🇦🇺)
-    filter: (?i)gemini|gpt|aws|iKuuu|(美国|英国1?)v4|_SG_|DE 02|英国 04|法国 06
-    exclude-filter: (?i)speednode|香港|hk|美国|澳大利亚|荷兰|美国0[1-6]|新加坡(0[1346789]|1[02])|日本(0[12356789]|1[02])|00[126]|vless新加坡02
+    filter: (?i)gemini|gpt|aws|iKuuu|github|🇷🇴|RO|罗马尼亚|🇹🇼|台湾|TW|瑞士|澳大利亚|_SG_|_JP_|ip5|DE 02|KR 02|GB 02|NL 02|US 0[35678]|USA 9|GBR 2|英国 0[24]|法国 06|德国01 4|荷兰01 4|美国-006|荷兰-001|洛杉矶-3|阿姆斯特丹-[26]|NL_speednode_000[58]|(美国|英国1?)v4|vless新加坡02
   - name: 国外媒体
     type: url-test
     url: http://www.gstatic.com/generate_204
