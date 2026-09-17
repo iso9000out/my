ec62960b8b075654523891d83bcc89c4a81f01a6
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-17 21:08:40 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -174,14 +174,14 @@ proxy-providers:
 #      enable: true
 #      interval: 3640
 #      url: http://www.gstatic.com/generate_204
-  ZiyouFly-Site:
-    type: http
-    url: https://s.imgki.com/fwCuV8Q
-    interval: 86442
-    health-check:
-      enable: true
-      interval: 3645
-      url: http://www.gstatic.com/generate_204
+#  ZiyouFly-Site失效了下次替换它:
+#    type: http
+#    url: https://s.imgki.com/fwCuV8Q
+#    interval: 86442
+#    health-check:
+#      enable: true
+#      interval: 3645
+#      url: http://www.gstatic.com/generate_204
   YouZiLite-Site:
     exclude-filter: 关键词|关键的词
     type: http
