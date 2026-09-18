ab81f6ac44aea1204369f368a0803f58821c521d
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-18 17:19:17 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -509,8 +509,8 @@ proxy-groups:
     max-failed-times: 3
     lazy: true
     include-all-providers: true
-    filter: (?i)gemini|aws|iKuuu|🇹🇼|台湾|tw|🇷🇴|罗马尼亚|ro|🇺🇸|美国|us|🇳🇱|荷兰|nl|🇩🇪|德国|de|🇬🇧|英国|gb|🇫🇷|法国|fr|🇯🇵|日本|jp|🇰🇷|韩国|kr|🇸🇬|新加坡|sg|🇹🇭|泰国|th|瑞士|澳大利亚
-    exclude-filter: (?i)speednode|gpt|https|🇭🇰|香港|hk
+    filter: (?i)gemini|aws|iKuuu|🇹🇼|台湾|tw|🇷🇴|罗马尼亚|ro|🇺🇸|美国|us|🇳🇱|荷兰|nl|🇩🇪|德国|de|🇨🇿|捷克|🇬🇧|英国|gb|🇫🇷|法国|fr|🇯🇵|日本|jp|🇰🇷|韩国|kr|🇸🇬|新加坡|sg|🇹🇭|泰国|th|瑞士|澳大利亚
+    exclude-filter: (?i)speednode|gpt|https|🇭🇰|香港|hk|ntls|mb
   - name: 国外媒体
     type: url-test
     url: http://www.gstatic.com/generate_204
@@ -520,7 +520,7 @@ proxy-groups:
     max-failed-times: 3
     lazy: true
     include-all-providers: true
-    filter: (?i)gemini|gpt|aws|iKuuu|speednode|https
+    filter: (?i)gemini|gpt|aws|iKuuu|speednode|https|ntls
     exclude-filter: (?i)香港|hk|中国|china|cn
   - name: 微软服务
     type: select
