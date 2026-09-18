46629c80593a06ba6e7a70abd7782ba5a7aab1d9
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-19 04:00:34 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -509,7 +509,7 @@ proxy-groups:
     max-failed-times: 3
     lazy: true
     include-all-providers: true
-    filter: (?i)gemini|gpt|ikuuu|https|anytls|vless|hy2
+    filter: (?i)gemini|gpt|ikuuu|https|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[456]|美国-08|荷兰 0[345789]|ip5|104|172|162|TLS|
   - name: 国外媒体
     type: url-test
     url: http://api.anthropic.com/v1/messages
@@ -520,7 +520,7 @@ proxy-groups:
     max-failed-times: 3
     lazy: true
     include-all-providers: true
-    filter: (?i)gemini|gpt|ikuuu|https|anytls|vless|hy2
+    filter: (?i)gemini|gpt|ikuuu|https|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[4569]|美国-08|美国 30|荷兰 0[345789]|ip5|104|172|162|0133|USA [39]|SG95
   - name: 微软服务
     type: select
     proxies:
