560e840369817b86020e7b4c021beb0576584ce6
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-19 13:12:28 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -509,7 +509,7 @@ proxy-groups:
     max-failed-times: 3
     lazy: true
     include-all-providers: true
-    filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[456]|美国-08|荷兰 0[345789]|ip5|104|172|162|TLS
+    filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[456]|美国-08|荷兰 0[345789]|ip5|TLS
     exclude-filter: (?i)香港
   - name: 国外媒体
     type: url-test
