d58acb7bec5f902263b36df6ac0b9644666b92f3
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-20 03:15:52 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -510,7 +510,7 @@ proxy-groups:
     lazy: true
     include-all-providers: true
     filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[456]|美国-08|荷兰 0[345789]|ip5|TLS
-    exclude-filter: (?i)香港|未知
+    exclude-filter: (?i)香港|中国|未知
     proxies:
       - 节点选择
       - 自动选优
@@ -525,7 +525,7 @@ proxy-groups:
     lazy: true
     include-all-providers: true
     filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[4569]|美国-08|美国 30|荷兰 0[345789]|ip5|0133|USA [39]|SG95
-    exclude-filter: (?i)香港|未知
+    exclude-filter: (?i)香港|中国|未知
     proxies:
       - 谷歌服务
   - name: 微软服务
