a19082ef7b887d4e91e3d2cd780d160fa4d6d2e9
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-20 03:42:41 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -498,7 +498,7 @@ proxy-groups:
     max-failed-times: 3
     lazy: true
     include-all-providers: true
-    exclude-filter: (?i)aws|ikuuu|gpt|gemini|🇭🇰|香港|hk|🇨🇳|中国|cn|china|其他|未知|重置|更新|下次|刷新|机场|剩余|电报|期|页|官网|交流|群组|账号|无法
+    exclude-filter: (?i)aws|gemini|gpt|ikuuu|🇭🇰|香港|hk|🇨🇳|中国|cn|china|其他|未知|重置|更新|下次|刷新|机场|剩余|电报|期|页|官网|交流|群组|账号|无法
   - name: 谷歌服务
     type: url-test
     url: https://generativelanguage.googleapis.com/
@@ -509,7 +509,7 @@ proxy-groups:
     max-failed-times: 3
     lazy: true
     include-all-providers: true
-    filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2
+    filter: (?i)aws|gemini|gpt|ikuuu|anytls|vless|hy2
     exclude-filter: (?i)香港|中国|未知
     proxies:
       - 节点选择
@@ -524,7 +524,7 @@ proxy-groups:
     max-failed-times: 3
     lazy: true
     include-all-providers: true
-    filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2
+    filter: (?i)aws|gemini|gpt|ikuuu|anytls|vless|hy2
     exclude-filter: (?i)香港|中国|未知
     proxies:
       - 谷歌服务
