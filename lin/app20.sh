eed4d17a75c4ce0108010107b7757300d8045240
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-19 22:17:19 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -508,13 +508,13 @@ proxy-groups:
     tolerance: 800
     max-failed-times: 3
     lazy: true
+    include-all-providers: true
+    filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[456]|美国-08|荷兰 0[345789]|ip5|TLS
+    exclude-filter: (?i)香港
     proxies:
       - 节点选择
       - 自动选优
       - 全局直连
-    include-all-providers: true
-    filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[456]|美国-08|荷兰 0[345789]|ip5|TLS
-    exclude-filter: (?i)香港
   - name: 国外媒体
     type: url-test
     url: https://api.anthropic.com/v1/messages
@@ -524,14 +524,14 @@ proxy-groups:
     tolerance: 800
     max-failed-times: 3
     lazy: true
+    include-all-providers: true
+    filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[4569]|美国-08|美国 30|荷兰 0[345789]|ip5|0133|USA [39]|SG95
+    exclude-filter: (?i)香港
     proxies:
       - 节点选择
       - 自动选优
       - 谷歌服务
       - 全局直连
-    include-all-providers: true
-    filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[4569]|美国-08|美国 30|荷兰 0[345789]|ip5|0133|USA [39]|SG95
-    exclude-filter: (?i)香港
   - name: 微软服务
     type: select
     proxies:
