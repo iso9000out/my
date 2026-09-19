637bc7e76494bb785abf8b96a03359d2d5061ba8
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-19 23:35:49 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -511,7 +511,7 @@ proxy-groups:
     lazy: true
     include-all-providers: true
     filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[456]|美国-08|荷兰 0[345789]|ip5|TLS
-    exclude-filter: (?i)香港
+    exclude-filter: (?i)香港|未知
     proxies:
       - 全局直连
       - 节点选择
@@ -527,7 +527,7 @@ proxy-groups:
     lazy: true
     include-all-providers: true
     filter: (?i)gemini|gpt|ikuuu|anytls|vless|hy2|美国 0[127]|美国 1[78]|美国 2[4569]|美国-08|美国 30|荷兰 0[345789]|ip5|0133|USA [39]|SG95
-    exclude-filter: (?i)香港
+    exclude-filter: (?i)香港|未知
     proxies:
       - 全局直连
       - 节点选择
