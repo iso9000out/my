87d918c6dd581f53f4b67a6cc1e127ea465a0d6f
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-20 01:47:39 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -457,7 +457,7 @@ proxy-providers:
 #      url: http://www.gstatic.com/generate_204
   XingJi-Site:
     type: http
-    url: https://s.imgki.com/0DEjjg5
+    url: https://s.imgki.com/C8SNzpZ
     interval: 86666
     health-check:
       enable: true
