4060a2fa19ffe92fdf0d45d34eaf630b6dfda8c1
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-20 21:01:11 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -110,14 +110,14 @@ proxy-providers:
       enable: true
       interval: 3600
       url: https://www.gstatic.com/generate_204
-   JiangJiang-Site:
-     type: http
-     url: https://s.imgki.com/wn3LHHA
-     interval: 43200
-     health-check:
-       enable: true
-       interval: 3605
-       url: https://www.gstatic.com/generate_204
+  JiangJiang-Site:
+    type: http
+    url: https://s.imgki.com/wn3LHHA
+    interval: 43200
+    health-check:
+      enable: true
+      interval: 3605
+      url: https://www.gstatic.com/generate_204
 #  RiPao-Git十几个全不通:
 #    type: http
 #    url: https://v1.mk/gUX6fUH
@@ -415,14 +415,14 @@ proxy-providers:
 #      enable: true
 #      interval: 3790
 #      url: https://www.gstatic.com/generate_204
-   o00o:
-     type: http
-     url: https://v1.mk/C9zAjfj
-     interval: 43431
-     health-check:
-       enable: true
-       interval: 3795
-       url: https://www.gstatic.com/generate_204
+  o00o:
+    type: http
+    url: https://v1.mk/C9zAjfj
+    interval: 43431
+    health-check:
+      enable: true
+      interval: 3795
+      url: https://www.gstatic.com/generate_204
 #  Firefoxmmx2-Git不通数量少暂时隐藏:
 #    type: http
 #    url: https://s.subcsub.com/CrvXoUi
