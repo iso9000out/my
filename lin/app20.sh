661c2947d02f08631882caf74dad3dd32edb3bb7
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-21 05:22:38 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -105,7 +105,7 @@ proxy-providers:
   own-Git:
     type: http
     url: https://cdn.gh-proxy.org/https://raw.githubusercontent.com/iso9000out/my/main/test/own
-    interval: 43200
+    interval: 172400
     health-check:
       enable: true
       interval: 3600
@@ -507,7 +507,7 @@ proxy-groups:
     timeout: 3000
     tolerance: 600
     max-failed-times: 3
-    #lazy: true
+    lazy: true
     #proxies:
       #- 节点选择
       #- 自动选优
@@ -522,7 +522,7 @@ proxy-groups:
     timeout: 2800
     tolerance: 600
     max-failed-times: 3
-    #lazy: true
+    lazy: true
     #proxies:
       #- 谷歌服务
     include-all-providers: true
