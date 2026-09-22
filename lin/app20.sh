94802640a93e9577fa3b9e498142eb66c9b13bbe
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-23 02:04:53 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -498,7 +498,7 @@ proxy-groups:
     max-failed-times: 3
     lazy: true
     include-all-providers: true
-    exclude-filter: (?i)aws|gemini|gpt|ikuuu|🇭🇰|香港|hk|🇨🇳|中国|cn|china|其他|未知|重置|更新|下次|刷新|机场|剩余|电报|期|页|官网|交流|群组|账号|无法
+    exclude-filter: (?i)aws|claude|gemini|gpt|ikuuu|🇭🇰|香港|hk|🇨🇳|中国|cn|china|其他|未知|重置|更新|下次|刷新|机场|剩余|电报|期|页|官网|交流|群组|账号|无法
   - name: 谷歌服务
     type: url-test
     url: https://generativelanguage.googleapis.com/
@@ -511,21 +511,14 @@ proxy-groups:
     #proxies:
       #- 节点选择
       #- 自动选优
+    include-all-providers: true
     filter: (?i)claude
   - name: 国外媒体
-    type: url-test
-    url: https://api.anthropic.com/v1/messages
-    expected-status: 401/403/404
-    interval: 800
-    timeout: 2800
-    tolerance: 600
-    max-failed-times: 3
-    lazy: true
+    type: select
     proxies:
-      - 谷歌服务
-    include-all-providers: true
-    filter: (?i)ikuuu
-    exclude-filter: (?i)未知|🇭🇰|香港|hk|🇨🇳|中国|cn|china|🇷🇺|俄罗斯|ru
+      - 节点选择
+      - 自动选优
+      - 全局直连
   - name: 微软服务
     type: select
     proxies:
@@ -721,9 +714,10 @@ rule-providers:
 
 rules:
   - DOMAIN-SUFFIX,gemini.gstatic.com,谷歌服务
-  - DOMAIN-SUFFIX,claude.com,国外媒体
+  - DOMAIN-SUFFIX,claude.com,谷歌服务
   - DOMAIN-SUFFIX,apis.google.com,节点选择
-  - DOMAIN-SUFFIX,challenges.cloudflare.com,节点选择
+  - DOMAIN-SUFFIX,challenges.cloudflare.com,谷歌服务
+  - DOMAIN-KEYWORD,nodebuf,谷歌服务
 
 # 以上特殊扭转摆正强调的
 
