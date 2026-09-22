47f33c495ce3c11d6fcb56d2031a319989d97606
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-23 03:01:02 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -717,6 +717,7 @@ rules:
   - DOMAIN-SUFFIX,claude.com,谷歌服务
   - DOMAIN-SUFFIX,apis.google.com,节点选择
   - DOMAIN-SUFFIX,challenges.cloudflare.com,谷歌服务
+  - DOMAIN-SUFFIX,nodebuf.com,谷歌服务
   - DOMAIN-KEYWORD,nodebuf,谷歌服务
 
 # 以上特殊扭转摆正强调的
