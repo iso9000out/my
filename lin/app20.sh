dd124bde7c03fb53b97bcb3d55422bf966e87dbe
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-22 17:00:02 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -491,7 +491,7 @@ proxy-groups:
     exclude-filter: 其他|未知|重置|更新|下次|刷新|机场|剩余|电报|期|页|官网|交流|群组|账号|无法
   - name: 自动选优
     type: url-test
-    url: http://www.gstatic.com/generate_204
+    url: https://www.gstatic.com/generate_204
     interval: 300
     timeout: 2000
     tolerance: 600
