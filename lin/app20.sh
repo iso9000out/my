adff28578f08f5e4066d90b343a0401e8fdb173e
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-23 04:33:14 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -1,4 +1,4 @@
-# date 2026-09-20
+# date 2026-09-23
 
 
 port: 7890
@@ -488,7 +488,7 @@ proxy-groups:
     proxies:
       - 自动选优
     include-all-providers: true
-    exclude-filter: 其他|未知|重置|更新|下次|刷新|机场|剩余|电报|期|页|官网|交流|群组|账号|无法
+    exclude-filter: claude|其他|未知|重置|更新|下次|刷新|机场|剩余|电报|期|页|官网|交流|群组|账号|无法
   - name: 自动选优
     type: url-test
     url: http://www.gstatic.com/generate_204
