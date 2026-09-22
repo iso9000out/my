d7802cdf21683537fd3060b80895801e68da5257
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-22 17:28:12 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -501,9 +501,9 @@ proxy-groups:
     exclude-filter: (?i)aws|gemini|gpt|ikuuu|🇭🇰|香港|hk|🇨🇳|中国|cn|china|其他|未知|重置|更新|下次|刷新|机场|剩余|电报|期|页|官网|交流|群组|账号|无法
   - name: 谷歌服务
     type: url-test
-    url: http://generativelanguage.googleapis.com/
+    url: https://generativelanguage.googleapis.com/
     expected-status: 401/403/404
-    interval: 320
+    interval: 900
     timeout: 3000
     tolerance: 600
     max-failed-times: 3
@@ -516,9 +516,9 @@ proxy-groups:
     exclude-filter: (?i)ikuuu|未知|其他|🇭🇰|香港|hk|🇨🇳|中国|cn|china|🇷🇺|俄罗斯|ru
   - name: 国外媒体
     type: url-test
-    url: http://api.anthropic.com/v1/messages
+    url: https://api.anthropic.com/v1/messages
     expected-status: 401/403/404
-    interval: 330
+    interval: 800
     timeout: 2800
     tolerance: 600
     max-failed-times: 3
