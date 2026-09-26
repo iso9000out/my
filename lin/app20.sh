ac8c11aa60649bd8f198504b4519ea4278e346ff
iso9000out <212241797+iso9000out@users.noreply.github.com>
2026-09-26 11:41:12 +0800

    Update app20.sh

--- lin/app20.sh
+++ lin/app20.sh
@@ -664,6 +664,13 @@ rule-providers:
     behavior: classical
     url: https://cdn.gh-proxy.org/https://raw.githubusercontent.com/iso9000out/my/main/Myrulesbai.list
     path: ./ruleset/Myrulesbai.yaml
+  Mygoogle:
+    type: http
+    format: text
+    interval: 1209
+    behavior: classical
+    url: https://cdn.gh-proxy.org/https://raw.githubusercontent.com/iso9000out/my/main/Mygoogle.list
+    path: ./ruleset/Mygoogle.yaml
   Gemini:
     type: http
     format: text
@@ -720,7 +727,7 @@ rules:
   - DOMAIN-SUFFIX,nodebuf.com,谷歌服务
   - DOMAIN-KEYWORD,nodebuf,谷歌服务
 
-
+  - RULE-SET,Mygoogle,谷歌服务
   - RULE-SET,LocalAreaNetwork,全局直连
   - RULE-SET,SteamCN,全局直连
   - RULE-SET,GoogleCN,全局直连
